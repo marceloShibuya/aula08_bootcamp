609d84ad38ff0c9ec797b1b75247fd225dc02b62
Marcelo Shibuya <marcelo.hshibuya@gmail.com>
2024-07-25 20:34:44 -0300

    adicionando a funcao de gerar dados no formato csv ou parquet

--- etl.py
+++ etl.py
@@ -3,8 +3,10 @@ import pandas as pd
 import os
 import glob
 
-# uma funcao de extract que le e consolida os json
 def extrair_dados_e_consolidar(caminho_pasta: str) -> pd.DataFrame:
+    """
+    uma funcao de extract que le e consolida os arquivos json
+    """
     # juntando todos os arquivos da pasta "data" que estão no formato json 
     arquivo_json = glob.glob(os.path.join(caminho_pasta, '*.json'))
     # percorrendo todas as linhas, inclusive os cabecalhos dos arquivos que estão no formato json
@@ -14,22 +16,32 @@ def extrair_dados_e_consolidar(caminho_pasta: str) -> pd.DataFrame:
     #print(df_total)
     return df_total
 
-# uma funcao que transforma
+
 def calcular_kpi_de_total_vendas(df_total: pd.DataFrame) -> pd.DataFrame:
+    """
+    uma funcao que transforma
+    """
     df_total["Total"] = df_total["Quantidade"] * df_total["Venda"]
     return df_total
 
 
-# uma funcao que da load em csv ou parquet
-
-
-
+def carregar_dados(df: pd.DataFrame, format_saida: list):
+    """
+    uma funcao que da load em csv ou parquet
+    """
+    for formato in format_saida:
+        if formato == "csv":
+            df.to_csv("dados.csv", index=False)
+        if formato == "parquet":
+            df.to_parquet("dados.parquet") 
 
 
 if __name__ == "__main__":
-    pasta = 'data'
+    pasta: str = 'data'
     tabela = extrair_dados_e_consolidar(pasta)
     df_transformado = calcular_kpi_de_total_vendas(tabela)
-    print(df_transformado)
+    tipo_arquivo: list = ["csv","parquet"]
+    carregar_dados(df_transformado, tipo_arquivo)
+   
 
 
